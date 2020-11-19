48db5f8da429fc7133dc0c1386fa274c255f0006
Erik Ho <eho61@my.bcit.ca>
2020-11-19 14:27:17 -0800

    Added stats screen

---
+++ src/components/ScoreItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>{{index+1}}.   {{score}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"ScoreItem",
+    props:{      
+        score:Number,
+        index:Number
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Stats.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        
+        <ViewButton text="Menu" target="Home"/>
+        <h1>Stats</h1>
+        <!-- pass histories and high scores here -->
+        <Highscores title="HighScores" v-bind:scores="scores" />
+        <Highscores title="Recent Runs" v-bind:scores="histories"/>
+    </div>
+</template>
+
+<script>
+    import Highscores from '../components/HighScores';
+    import ViewButton from '../components/ViewButton';
+    import Storage from '../utils/Storage';
+    
+    export default{
+        name: 'Stats',
+        components: {
+            Highscores,
+            ViewButton         
+        },
+        data(){
+            return{
+                scores:[],
+                histories:[]
+            }
+        },     
+        created: function(){
+            let storage = Storage.getStorage();
+            var hist = storage.getHistory();
+            var hscores = storage.getHighscores();
+            var size = 5;
+            this.scores = hscores.slice(0, size);
+            this.histories = hist.slice(0, size);        
+            console.log("the score is: " + this.scores);
+            console.log("this history is: " + this.histories);
+        }       
+    }
+</script>
+
+<style>
+
+</style>
