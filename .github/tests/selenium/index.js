496cd3c4bba51f9a0bc5076e3711888f016af9e9
Thomas Bui <thomasbui198@gmail.com>
2020-12-03 11:50:19 -0800

    Remove code to wait for server

--- .github/tests/selenium/index.js
+++ .github/tests/selenium/index.js
@@ -1,24 +1,17 @@
 const selenium = require("selenium-webdriver")
 const firefox = require("selenium-webdriver/firefox")
 const util = require("./util")
-const fsPromise = require("fs").promises
+const fsPromise = require("fs/promises")
 const path = require("path")
 const child_process = require("child_process")
-const { reject } = require("core-js/fn/promise")
 
 // must turn on the server before running script
 async function main() {
   // get the homeDir passed in as arg
   let screenshotFolderPath = process.argv[2]
 
-  try {
-    let stdout = await startServer()
-    console.log(stdout)
-  } catch(e) {
-    console.log(e)
-    return
-  }
-  
+  // don't wait cause it's a server
+  child_process.exec("npm run serve")
   let driver = util.createSeleniumInstance()
   try {
     await driver.get("http://localhost:8080/")
@@ -37,7 +30,6 @@ async function startServer() {
         reject(err)
         return
       }
-      resolve(stdout)
     })
   })
 }
