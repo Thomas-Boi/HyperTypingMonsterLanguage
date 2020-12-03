77667eda241a12c54fa16069658a17fdb5c405e6
Thomas Bui <thomasbui198@gmail.com>
2020-12-03 11:46:47 -0800

    Added child process to start server

--- .github/tests/selenium/index.js
+++ .github/tests/selenium/index.js
@@ -3,15 +3,25 @@ const firefox = require("selenium-webdriver/firefox")
 const util = require("./util")
 const fsPromise = require("fs").promises
 const path = require("path")
+const child_process = require("child_process")
+const { reject } = require("core-js/fn/promise")
 
 // must turn on the server before running script
 async function main() {
   // get the homeDir passed in as arg
   let screenshotFolderPath = process.argv[2]
+
+  try {
+    let stdout = await startServer()
+    console.log(stdout)
+  } catch(e) {
+    console.log(e)
+    return
+  }
   
   let driver = util.createSeleniumInstance()
   try {
-    await driver.get("https://localhost:8080/")
+    await driver.get("http://localhost:8080/")
     let pngFile = await driver.takeScreenshot()
     let pngFilePath = path.join(screenshotFolderPath, "home.png")
     await fsPromise.writeFile(pngFilePath, pngFile)
@@ -20,8 +30,20 @@ async function main() {
   }
 }
 
+async function startServer() {
+  return new Promise((resolve, reject) => {
+    child_process.exec("npm run serve", (err, stdout) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(stdout)
+    })
+  })
+}
+
 async function test_tutorial(driver) {
-  
+
 }
 
 main()
